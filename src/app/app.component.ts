3e57f94227c793c86476d43ff7f930fef3cb1ec5
Darryl Walker <darryl.walker@jacobs.com>
2022-06-29 11:49:58 +0100

    Partial solution for angular specific challenges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-dark-mode-test';
+  constructor(private formBuilder: FormBuilder) {}
+
+  title = 'Angular Dark Mode Test';
+  darkMode = false;
+
+  demoForm = this.formBuilder.group({
+    name: '',
+    select: '',
+  });
+
+  public toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+  }
+
+  public onSubmit() {
+    // Process checkout data here
+    console.warn('Your order has been submitted', this.demoForm.value);
+  }
 }
